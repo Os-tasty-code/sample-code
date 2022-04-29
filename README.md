41ad5c33c1b03105484d0c31619d29af0000bc76
O Dodart <43223613+Os-tasty-code@users.noreply.github.com>
2022-04-29 18:09:34 -0400

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Open this page at [https://os-tasty-code.github.io/sample-code/](https://os-tasty-code.github.io/sample-code/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/os-tasty-code/sample-code** and import
+
+## Edit this project ![Build status badge](https://github.com/os-tasty-code/sample-code/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/os-tasty-code/sample-code** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/os-tasty-code/sample-code/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,73 @@
+//working with JavaScript libraries while learning JavaScript
+
+//code in JavaScript
+//work with the library
+
+//math
+//variable
+//if statements
+
+//function inside of a class
+//function - anything that does something
+
+//LOOP REQUIREMENTS
+//1. start position
+//2. stopping condition
+//3. method of change
+
+//for x in range(5)
+//    x = 0
+//    x == 5
+//    x is increasing by 1
+
+//for(starting position; stopping condition; change)
+//var | let --> create variables
+
+//stopping condition
+//    needs to false
+//    true --> false
+
+//infinite loop
+//  --> BAD 
+//  * never reaching the stopping condition
+//  * not changing your variable (while loops)
+
+//arrays - collection/lists of values
+
+//data types = types of information
+//let array = [" a ", " b ", " c ", " d ", " e "];
+// accessing an array: nameOfArray[positionOfTheValue]   
+// for(let x = 0; x < 5; x = x + 1) {
+//     basic.showString(array[x]);
+// }
+
+
+
+basic.showLeds(`
+. # # # .
+. # # # .
+. . # . .
+# # # # #
+. . . . .
+`)
+basic.showLeds(`
+# # # # #
+. . # . .
+. . # . .
+. # . # .
+# . . . #
+`)
+basic.showLeds(`
+. # # # .
+# . # . #
+. . # . .
+# # # # .
+. . . . #
+`)
+
+//let grr = game.createSprite(1, 3)
+//
+
+input.onButtonPressed(Button.A, function() {
+    //code for when A Button is pressed
+})
